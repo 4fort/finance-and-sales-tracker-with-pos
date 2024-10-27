d40a51143d2f06146aface5056b257805c69796b
Ronin Allyson Manso <ronin@localhost.localdomain>
2024-10-27 16:06:12 +0800

    Refactor landing page

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -10,10 +10,10 @@ export function Header() {
     <>
       <header className="px-10 lg:px-40 h-24 flex items-center bg-transparent absolute top-0 left-0 right-0 z-20">
         <Link className="flex items-center justify-center" href="#">
-          <ShoppingCart className="h-6 w-6 mr-2 text-black" />
-          <span className=" text-xl font-bold text-black">AdminCommerce</span>
+          <ShoppingCart className="h-6 w-6 mr-2 text-white" />
+          <span className=" text-xl font-bold text-white">AdminCommerce</span>
         </Link>
-        <nav className="ml-auto hidden md:flex gap-4 sm:gap-6 text-black">
+        <nav className="ml-auto hidden md:flex gap-4 sm:gap-6 text-white">
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
             href="#features">
--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -2,19 +2,33 @@ import { MoveRight, PhoneCall } from 'lucide-react'
 import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
-
+import HeroImage from '../../../public/hero-image-3.jpg'
 export const Hero = () => {
   const router = useRouter()
 
   return (
     <div className="w-full overflow-hidden relative z-10">
-      <div className="container mx-auto">
+      {/* Background Image */}
+      <div className="absolute inset-0 z-0">
+        <Image
+          src={HeroImage} // Path to your overlay image
+          alt="Background Image"
+          layout="fill" // Makes the image cover the entire container
+          objectFit="cover" // Ensures the image scales properly
+          className="opacity-50" // Adjust the opacity for a subtle overlay
+        />
+      </div>
+
+      {/* Dark Color Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-70 z-0"></div>
+
+      <div className="container mx-auto relative z-10">
         <div className="flex min-h-[90vh] gap-8 py-20 lg:py-40 items-center justify-center flex-col">
           <div className="flex gap-4 flex-col">
-            <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-bold text-slate-900 dark:text-white">
+            <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-bold text-white">
               Supercharge Your E-commerce Business
             </h1>
-            <p className="text-lg md:text-xl leading-relaxed tracking-tight text-slate-900 dark:text-white max-w-2xl text-center">
+            <p className="text-lg md:text-xl leading-relaxed tracking-tight  text-white max-w-2xl text-center">
               Powerful admin tools to manage your online store, boost sales, and
               delight customers.
             </p>
